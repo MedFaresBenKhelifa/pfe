e73ec71c4b46cd6385996c07b0f59bbd49b2df69
MedFaresBenKhelifa <farestrabelsi124@gmail.com>
2025-03-02 14:43:53 +0100

    ajout demande

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { signupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ChangerMdpComponent } from './components/changer-mdp/changer-mdp.component';
+import { DemandeComponent } from './components/demande/demande.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'LogIn', pathMatch: 'full' },
   { path: 'LogIn', component: LoginComponent },
   { path: 'SignUp', component: signupComponent },
   { path : 'Dashboard', component: DashboardComponent},
+  { path : 'Demande', component: DemandeComponent},
   { path : 'change', component: ChangerMdpComponent},
   { path: '**', component: LoginComponent },
 ];
